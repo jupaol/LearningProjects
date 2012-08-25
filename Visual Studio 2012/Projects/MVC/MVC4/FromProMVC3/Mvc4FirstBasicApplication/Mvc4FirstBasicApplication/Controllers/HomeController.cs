84fef176fe9c65a229c47f3daf37bd2fb9febb55
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-08-25 00:22:07 -0500

    Adding initial solutions

---
+++ Visual Studio 2012/Projects/MVC/MVC4/FromProMVC3/Mvc4FirstBasicApplication/Mvc4FirstBasicApplication/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Mvc4FirstBasicApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Mvc4FirstBasicApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            var greeting = DateTime.Now.Hour >= 12 ? "Good afternoon" : "Good morning";
+
+            this.ViewBag.Greeting = greeting;
+
+            return View();
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = -1, Location = OutputCacheLocation.None, NoStore = true, VaryByParam = "none")]
+        public ViewResult Register()
+        {
+            HttpContext.Response.Expires = -1;
+            HttpContext.Response.Cache.SetNoServerCaching();
+            HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            HttpContext.Response.Cache.SetAllowResponseInBrowserHistory(false);
+            HttpContext.Response.CacheControl = "no-cache";
+            HttpContext.Response.Cache.SetNoStore();
+
+            return View();
+        }
+
+        [HttpPost]
+        [OutputCache(Duration = -1, Location = OutputCacheLocation.None, NoStore = true, VaryByParam = "none")]
+        public ActionResult Register(RegisterModel model)
+        {
+            HttpContext.Response.Expires = -1;
+            HttpContext.Response.Cache.SetNoServerCaching();
+            HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            HttpContext.Response.Cache.SetAllowResponseInBrowserHistory(false);
+            HttpContext.Response.CacheControl = "no-cache";
+            HttpContext.Response.Cache.SetNoStore();
+
+            if (!this.ModelState.IsValid)
+            {
+                return View();
+            }
+
+            this.Session["m"] = model;
+
+            return RedirectToAction("Thanks", new { id = 4 });
+        }
+
+        [OutputCache(Duration = -1, Location = OutputCacheLocation.None, NoStore = true, VaryByParam = "none")]
+        public ActionResult Thanks(int id)
+        {
+            HttpContext.Response.Expires = -1;
+            HttpContext.Response.Cache.SetNoServerCaching();
+            HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            HttpContext.Response.Cache.SetAllowResponseInBrowserHistory(false);
+            HttpContext.Response.CacheControl = "no-cache";
+            HttpContext.Response.Cache.SetNoStore();
+
+            return View(this.Session["m"] as RegisterModel);
+        }
+    }
+}
---
+++ Visual Studio 2012/Projects/MVC/MVC4/FromProMVC3/Mvc4FirstBasicApplication/Mvc4FirstBasicApplication/Models/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4FirstBasicApplication.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+        [MinLength(5)]
+        [MaxLength(10)]
+        public string NickName { get; set; }
+        [Range(18, 80)]
+        public int? Age { get; set; }
+        public bool? WillAttend { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+    }
+}

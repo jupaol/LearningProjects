a287e8b980dc9ba2e1b4f5414b4dac5423eb65a1
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-09-16 02:38:18 -0500

    adding session state examples

---
+++ VS 2012/src/WebForms/MSTS 70-515/Msts/Topics/Chapter03/Lesson03 - ServerSideState/UsingSessionState_StateServer.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Msts.Topics.Chapter03.Lesson03___ServerSideState
+{
+    public partial class UsingSessionState_StateServer : System.Web.UI.Page
+    {
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            this.msg.Text = this.Session["cs"] != null ? this.Session["cs"].ToString() : string.Empty;
+        }
+
+        protected void updateSessionState_Click(object sender, EventArgs e)
+        {
+            this.Session["cs"] = DateTime.Now;
+        }
+    }
+}

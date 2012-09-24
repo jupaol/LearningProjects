a8dd023f61f8adee547a001323f931c0061a43c2
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-09-23 20:44:23 -0500

    Added brandable web project

---
+++ VS 2012/src/Blog/BrandableApplication/BrandableApplication/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BrandableApplication.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="header" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="mainContent" runat="server">
+    <div class="post">
+		<h2 class="title">
+            dede
+		</h2>
+		<p class="byline">
+		</p>
+		<div class="entry">
+		</div>
+		<p class="links">
+        </p>
+	</div>
+</asp:Content>
+<asp:Content runat="server" ID="Content3" ContentPlaceHolderID="sidebarContent">
+
+</asp:Content>
---
+++ VS 2012/src/Blog/BrandableApplication/BrandableApplication/Global.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BrandableApplication
+{
+    public partial class Global : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void layouts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Response.Redirect(this.Request.RawUrl);
+        }
+
+        protected void colors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Response.Redirect(this.Request.RawUrl);
+        }
+
+        protected void languages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Response.Redirect(this.Request.RawUrl);
+        }
+    }
+}

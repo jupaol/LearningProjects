cb42d9eeb84ff291cca1961f380857b7fc5c7814
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-10-06 06:13:23 -0500

    Added AjaxControlToolkit

--- VS 2012/src/WebForms/MSTS 70-515/Msts/Topics/Chapter02/Lesson01 - MasterPages/GlobalMaster.Master.designer.cs
+++ VS 2012/src/WebForms/MSTS 70-515/Msts/Topics/Chapter02/Lesson01 - MasterPages/GlobalMaster.Master.designer.cs
@@ -49,13 +49,13 @@ namespace Msts.Topics.Chapter02.Lesson01___MasterPages {
         protected global::System.Web.UI.WebControls.WebParts.WebPartManager wpm;
         
         /// <summary>
-        /// ScriptManager1 control.
+        /// ToolkitScriptManager1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        protected global::AjaxControlToolkit.ToolkitScriptManager ToolkitScriptManager1;
         
         /// <summary>
         /// ddlLayouts control.

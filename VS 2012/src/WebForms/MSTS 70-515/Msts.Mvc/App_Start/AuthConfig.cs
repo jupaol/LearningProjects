bc854ed270199ed9f742e8043991abcec1d5adde
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-11-04 06:00:05 -0600

    Configured OAuth for the MVC App

--- VS 2012/src/WebForms/MSTS 70-515/Msts.Mvc/App_Start/AuthConfig.cs
+++ VS 2012/src/WebForms/MSTS 70-515/Msts.Mvc/App_Start/AuthConfig.cs
@@ -14,19 +14,27 @@ namespace Msts.Mvc
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "00000000480DDA42",
+                clientSecret: "sJ7x8py-mvo45tHaEFX8dunNEQXVQfGc",
+                displayName: "Microsoft",
+                extraData: new Dictionary<string, object> { { "Icon", "~/Images/live.jpg" } });
 
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            OAuthWebSecurity.RegisterTwitterClient(
+                consumerKey: "jspfwqPm4kvtwpt8PppBBA",
+                consumerSecret: "RJIwjgW6Bo4wUNr32nlCOIcISipv5Hi2NTu4u6kMrts",
+                displayName: "Twitter",
+                extraData: new Dictionary<string, object> { { "Icon", "~/Images/twitter.png" } });
 
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "459944857377239",
+                appSecret: "e05079e0019192399422c81c2bddbeb5",
+                displayName: "Facebook",
+                extraData: new Dictionary<string, object> { { "Icon", "~/Images/facebook.jpg" } });
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient(
+                displayName: "Google",
+                extraData: new Dictionary<string, object> { { "Icon", "~/Images/google.png" } });
         }
     }
 }
--- VS 2012/src/WebForms/MSTS 70-515/Msts.Mvc/Models/AccountModels.cs
+++ VS 2012/src/WebForms/MSTS 70-515/Msts.Mvc/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace Msts.Mvc.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("Msts")
         {
         }
 

9761dbecffd1259eae6d8853ae508461ab16111a
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2013-08-21 15:02:23 -0400

    Injecting resolver into api controller

--- VS 2012/src/Ninject/NinjectWithInMvcAndWcf/NinjectWithInMvcAndWcf/Controllers/ValuesController.cs
+++ VS 2012/src/Ninject/NinjectWithInMvcAndWcf/NinjectWithInMvcAndWcf/Controllers/ValuesController.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 using NinjectWithInMvcAndWcf.Models;
+using NinjectWithInMvcAndWcf.Services;
 using Ploeh.AutoFixture;
 
 namespace NinjectWithInMvcAndWcf.Controllers
 {
     public class ValuesController : ApiController
     {
+        private readonly IContextResolver _contextResolver;
+
+        public ValuesController(IContextResolver contextResolver)
+        {
+            _contextResolver = contextResolver;
+        }
+
         public IList<Person> Get()
         {
+            if (_contextResolver == null)
+            {
+                throw new ArgumentNullException("_contextResolver");
+            }
+
             var fixture = new Fixture();
 
             return new[]

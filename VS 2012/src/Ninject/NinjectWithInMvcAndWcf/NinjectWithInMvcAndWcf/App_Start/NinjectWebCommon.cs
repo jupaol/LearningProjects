a09a74620545e04424fa115df136ff1799b05aed
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2013-08-21 14:49:08 -0400

    Fixing using statements

--- VS 2012/src/Ninject/NinjectWithInMvcAndWcf/NinjectWithInMvcAndWcf/App_Start/NinjectWebCommon.cs
+++ VS 2012/src/Ninject/NinjectWithInMvcAndWcf/NinjectWithInMvcAndWcf/App_Start/NinjectWebCommon.cs
@@ -1,21 +1,14 @@
-using CommonServiceLocator.NinjectAdapter.Unofficial;
-using Microsoft.Practices.ServiceLocation;
-using NinjectWithInMvcAndWcf.Services;
+using System;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(NinjectWithInMvcAndWcf.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(NinjectWithInMvcAndWcf.App_Start.NinjectWebCommon), "Stop")]
 
 namespace NinjectWithInMvcAndWcf.App_Start
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Extensions.Conventions;
-    using Ninject.Web.Common;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();

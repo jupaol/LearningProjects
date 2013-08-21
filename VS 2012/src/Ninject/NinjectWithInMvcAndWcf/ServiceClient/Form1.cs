545265de492803198bdd4719495fdfa02ccdc721
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2013-08-21 15:52:31 -0400

    Adding windows forms client application

---
+++ VS 2012/src/Ninject/NinjectWithInMvcAndWcf/ServiceClient/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+using NinjectShared;
+
+namespace ServiceClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string resFromWcf;
+
+            using (var channelFactory = new ChannelFactory<ILoggingService>("MyLoggingServiceEndPoint"))
+            {
+                var proxy = channelFactory.CreateChannel();
+
+                resFromWcf = proxy.DoWork();
+            }
+
+            
+        }
+    }
+}

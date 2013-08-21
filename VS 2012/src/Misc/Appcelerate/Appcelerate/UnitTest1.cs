9d6d566225779fe80356c8a640ac7e65550d61db
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2013-08-21 13:46:45 -0400

    Adding appccelerate tests

---
+++ VS 2012/src/Misc/Appcelerate/Appcelerate/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Appccelerate.EventBroker;
+using Appccelerate.EventBroker.Handlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Appcelerate
+{
+    public class Publisher
+    {
+        [EventPublication("de")]
+        public event EventHandler SimpleEvent;
+
+        public void FireSimpleEvent()
+        {
+            SimpleEvent(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber
+    {
+        [EventSubscription(
+            "de",
+            typeof(OnPublisher))]
+
+        public void SimpleEvent(object sender, EventArgs e)
+        {
+            // do something useful or at least funny
+            Console.WriteLine("handled");
+        }
+    }
+
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var eventBroker = new EventBroker();
+
+            var publisher = new Publisher();
+            eventBroker.Register(publisher);
+
+            var subscriber = new Subscriber();
+            eventBroker.Register(subscriber);
+
+            publisher.FireSimpleEvent();
+        }
+    }
+}

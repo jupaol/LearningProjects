6e1823c62970e5b7f211a39af64698403dabb563
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2013-08-22 14:50:47 -0400

    Adding EF foreign key tests

---
+++ VS 2012/src/EntityFramework/ForeignKeys/ForeignKeys/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ForeignKeys
+{
+    [TestClass]
+    public class ForeignKeyTests
+    {
+        [TestMethod]
+        public void it_should_map_a_foreign_key()
+        {
+        }
+    }
+}

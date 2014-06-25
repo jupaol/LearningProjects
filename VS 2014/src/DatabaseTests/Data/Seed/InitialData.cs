32fecfdd37d72378fddb8c4cb370fa6cac253077
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2014-06-25 01:50:54 -0400

    Configuring seed data

--- VS 2014/src/DatabaseTests/Data/Seed/InitialData.cs
+++ VS 2014/src/DatabaseTests/Data/Seed/InitialData.cs
@@ -1,9 +1,12 @@
-﻿namespace Data.Seed
+﻿using Data.SeedExtensions;
+
+namespace Data.Seed
 {
     public class DataProvider
     {
         public static void PopulateData(BlogContext context)
         {
+            var saganAuthor = context.Authors.AddSeed(context, "JP", "jp@someemail.com");
             
         }
     }

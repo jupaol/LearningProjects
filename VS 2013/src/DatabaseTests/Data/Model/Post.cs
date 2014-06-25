5d3334b4b884d179c8bc205c637ded4cf034d609
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2014-06-25 13:03:50 -0400

    Renaming

---
+++ VS 2013/src/DatabaseTests/Data/Model/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Model
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid PostId { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        public Guid BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ VS 2013/src/DatabaseTests/Data/Seed/InitialData.cs
@@ -0,0 +1,13 @@
+﻿using Data.SeedExtensions;
+
+namespace Data.Seed
+{
+    public class DataProvider
+    {
+        public static void PopulateData(BlogContext context)
+        {
+            var saganAuthor = context.Authors.AddSeed(context, "JP", "jp@someemail.com");
+            
+        }
+    }
+}
